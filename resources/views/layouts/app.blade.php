ecb5d664239da0b431acbbe8aa78c65f4709a834
JennPride <Jennpride08@gmail.com>
2017-08-04 17:21:15 -0400

    Spacing
    Taking away unnecessary spacing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,8 +87,6 @@
 		<!-- /.container-fluid -->
 	</nav>
     @yield('content')
-
-
 		    <!-- JavaScripts -->
 				<script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.min.css"></script>
 		    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
